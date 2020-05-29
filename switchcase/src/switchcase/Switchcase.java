e189cee8389a3691000fcde60d2f50154824bdbb
sakthi <59285185+Sakthi3016@users.noreply.github.com>
2020-05-29 12:42:18 +0530

    using switch case
    in java

---
+++ switchcase/src/switchcase/Switchcase.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package switchcase;
+
+/**
+ *
+ * @author SAKTHI
+ */
+public class Switchcase {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int number=20;
+        switch(number){  
+    //Case statements  
+    case 10: System.out.println("10");  
+    break;  
+    case 20: System.out.println("20");  
+    break;  
+    case 30: System.out.println("30");  
+    break;  
+    //Default case statement  
+    default:System.out.println("Not in 10, 20 or 30");  
+    }  
+    }
+    
+}
